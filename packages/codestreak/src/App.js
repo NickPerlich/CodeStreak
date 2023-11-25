c65e36cf37bffbc6e4492edfe0c10244fcda4665
NickPerlich <nicholasperlich2003@gmail.com>
2023-11-24 20:41:46 -0800

    made it so the message returned in myapp comes from the endpoint in my backend

--- packages/codestreak/src/App.js
+++ packages/codestreak/src/App.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+function getBackendMessage() {
+    const promise = fetch('http://localhost:3000');
+    return promise
+}
 
 function App() {
+    const [message, setMessage] = useState('Loading...');
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const result = await getBackendMessage();
+                const data = await result.json();
+                setMessage(data.message);
+            } catch(error) {
+                console.log(error);
+            }
+        }
+
+        fetchData();
+    }, []);
+    
     return (
         <div>
-            Hello World!
+            {message}
         </div>
     )
 }
